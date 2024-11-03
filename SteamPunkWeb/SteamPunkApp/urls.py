0a97ac84765022945253815d1fadf5477730c40c
IbrahHaroon <ih62@njit.edu>
2024-11-03 15:45:28 -0500

    Updated views and urls

--- SteamPunkWeb/SteamPunkApp/urls.py
+++ SteamPunkWeb/SteamPunkApp/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.mainView, name="mainView")
-
+    path("", views.mainView, name="mainView"),
+    path('about_page/', views.about_page, name='about_page'),
+    path('base/', views.base, name='base'),
+    path('chat_page/', views.chat_page, name='chat_page'),
+    path('home/', views.home, name='home'),
+    path('transition_page', views.transition_page, name='transition_page'),
+    path('Tutorial_page', views.Tutorial_page, name='Tutorial_page'),
+    path("welcome_page", views.welcome_page, name='welcome_page'),
 ]
--- SteamPunkWeb/SteamPunkApp/views.py
+++ SteamPunkWeb/SteamPunkApp/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def mainView(request):
     return render(request, "home.html")
+
+def about_page(request):
+    return render(request, 'SteamPunkWeb/SteamPunkApp/templates/about_page.html')
+
+def base(request):
+    return render(request, 'SteamPunkWeb/SteamPunkApp/templates/base.html')
+
+def chat_page(request):
+    return render(request, 'SteamPunkWeb/SteamPunkApp/templates/chat_page.html')
+
+def home(request):
+    return render(request, 'SteamPunkWeb/SteamPunkApp/templates/home.html')
+
+def transition_page(request):
+    return render(request, 'SteamPunkWeb/SteamPunkApp/templates/transition_page.html')
+
+def Tutorial_page(request):
+    return render(request, 'SteamPunkWeb/SteamPunkApp/templates/Tutorial_page.html')
+
+def welcome_page(request):
+    return render(request, 'SteamPunkWeb/SteamPunkApp/templates/welcome_page.html')
